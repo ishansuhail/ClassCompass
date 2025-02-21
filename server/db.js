583a9d4fb9ba3eb8d5f244bb6e2dea038e2ac1d3
Aiden Drover-Mattinen <adrovermattinen@gmail.com>
2025-02-21 17:42:19 -0500

    ensuring we no longer print the supabase key

--- server/db.js
+++ server/db.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 
 const { createClient } = require('@supabase/supabase-js');
 
-console.log('Supabase Key:', process.env.SUPABASE_KEY);
-
 const supabaseUrl = 'https://rayjpvhzhwpxioqrcbdw.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
