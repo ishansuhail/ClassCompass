bc4da7b610fcafabbbeb9c1a52270829ae3e9a3b
Aiden Drover-Mattinen <adrovermattinen@gmail.com>
2025-02-28 18:56:43 -0500

    Adding initial version of the home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CoursePage from "./pages/CourseReview";
 import Search from "./pages/Search";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/CourseReview" element={<CoursePage />} />
           <Route path="/Search" element={<Search/>} />
+          <Route path="/Home" element={<Home/>} />
         </Routes>
       </div>
     </Router>
